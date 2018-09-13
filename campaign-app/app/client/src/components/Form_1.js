b71df38217f77adcad2e8f3142ada3aeba45fe3f
Omar Donnadieu Mercado <donnadieu@omardoncomputer.home>
2018-09-13 16:57:39 -0400

    Change form

--- campaign-app/app/client/src/components/Form_1.js
+++ campaign-app/app/client/src/components/Form_1.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Form_1 = () => {
   return(
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSewkjAusJYUyJz66HYytzQQdj1-KvjP9zxPUdqV75Qo79CGWA/viewform?embedded=true" width="640" height="907" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdqKDZGQa1TlVvVqvFTk-HGFL8A2qex69cM8ppiBk3sXLkdOw/viewform?embedded=true" width="640" height="1453" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
   )
 }
 
